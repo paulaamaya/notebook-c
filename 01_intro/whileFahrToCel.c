4745c7c3377ed3791622d960905df836ae3ef783
Paula Amaya <paula.amaya@ucalgary.ca>
2023-10-31 08:56:45 -0600

    Chnaged folder naming conversion

--- 01_intro/whileFahrToCel.c
+++ 01_intro/whileFahrToCel.c
@@ -12,11 +12,10 @@ main(){
 
     fahr = lower;
 
-    printf("Fahrenheit\tCelsius");
     while (fahr <= upper)
     {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f\t%6.2f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr += step;
     }
 }
